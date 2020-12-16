b37f269efbd5dc26a1558ecc2697791b34c0ea9c
ychitran <ychi1234@gmail.com>
2020-12-16 11:09:37 +0700

    Add Interfacecomparator folder

---
+++ interfacecomparator/Circle.php
@@ -0,0 +1,33 @@
+<?php
+
+class Circle
+{
+    public $radius;
+    public $name;
+
+    public function __construct($name, $radius)
+    {
+        $this->radius = $radius;
+        $this->name = $name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setRadius($radius)
+    {
+        $this->radius = $radius;
+    }
+
+    public function getRadius()
+    {
+        return $this->radius;
+    }
+}
