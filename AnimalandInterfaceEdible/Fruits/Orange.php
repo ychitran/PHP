648475714928051f46dfaa60b0a9a8844f1e691d
ychitran <ychi1234@gmail.com>
2020-12-21 09:08:10 +0700

    Add AnimalandInterfaceEdible folder

---
+++ AnimalandInterfaceEdible/Fruits/Orange.php
@@ -0,0 +1,12 @@
+<?php
+
+include_once(dirname(__FILE__) . '/../AbstractClass/Fruit.php');
+
+class Orange extends Fruit
+{
+
+    public function howToEat()
+    {
+        return "Orange could be juiced";
+    }
+}
